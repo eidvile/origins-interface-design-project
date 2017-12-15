4032d35b8d0a109974c96a0aaf904feebfc1913b
eidvile <34555804+eidvile@users.noreply.github.com>
2017-12-15 08:18:41 +0100

    Add files via upload

---
+++ templates/logout.php
@@ -0,0 +1,10 @@
+<div id="logout-container" class="close-these-popups">
+	<div id="close-log-out-container" class="fa fa-times close-popup-onclick-elements"></div>
+	<div id="account-name"></div>
+	<div id="account-email"></div>
+	<div id="logout-account-btn-container">
+		<div class="buttons links-active" data-href="my-account.php" id="my-account-btn">My account</div>
+		<div class="buttons" class="links-active" id="log-out-btn">Log out</div>
+	</div>
+	
+</div>
